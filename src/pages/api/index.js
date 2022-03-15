3306280f3ea9da24be2e8067d04178775af45929
Thor Anderson <ta.anderson@gmail.com>
2022-03-14 21:00:58 -0600

    Added some Amplify config to api route

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,4 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Amplify } from "aws-amplify"
+import config from '../../aws-exports'
+
+Amplify.configure(config)
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
