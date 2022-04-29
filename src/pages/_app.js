902cbdc327f660ec390b57237e4fd9cbc14de420
Thor Anderson <ta.anderson@gmail.com>
2022-04-29 17:30:32 -0600

    First attempt at Using Figma with UI LIbrary

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,49 +1,51 @@
 import { Amplify, AuthModeStrategyType } from 'aws-amplify'
 import ResponsiveAppBar from '../components/ResponsiveAppBar'
 import config from '../aws-exports'
-import { Authenticator } from '@aws-amplify/ui-react'
+import { Authenticator, AmplifyProvider } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import '../styles/globals.css'
 
 Amplify.configure({
   ...config,
   DataStore: {
-    authModeStrategyType: AuthModeStrategyType.MULTI_AUTH
-  }
+    authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
+  },
 })
 
 const formFields = {
   signUp: {
     email: {
-      order:1
+      order: 1,
     },
     name: {
       placeholder: 'First Name',
-      order: 2
+      order: 2,
     },
     family_name: {
       placeholder: 'Last Name',
-      order: 3
+      order: 3,
     },
     password: {
-      order: 4
+      order: 4,
     },
     confirm_password: {
-      order: 5
-    }
+      order: 5,
+    },
   },
- }
+}
 
-function MyApp({ Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
   return (
-    <Authenticator variation="modal" formFields={formFields}>
-      {({ signOut, user }) => (
-        <>
-          <ResponsiveAppBar user={user} signOut={signOut} />
-          <Component {...pageProps} />
-        </>
-      )}
-    </Authenticator>
+    <AmplifyProvider>
+      <Authenticator variation="modal" formFields={formFields}>
+        {({ signOut, user }) => (
+          <>
+            <ResponsiveAppBar user={user} signOut={signOut} />
+            <Component {...pageProps} />
+          </>
+        )}
+      </Authenticator>
+    </AmplifyProvider>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import { useAuthenticator } from '@aws-amplify/ui-react'
+import { 
+  CardG 
+} from '../ui-components';
 
 export default function Home() {
   const { user } = useAuthenticator((context) => [context.user]);
@@ -13,6 +16,7 @@ export default function Home() {
 
       <main>
         <h1>Welcome to my Next.js App {user.attributes.name} {user.attributes.family_name} of {user.attributes.email}!</h1>
+        <CardG />
       </main>
     </div>
   )
